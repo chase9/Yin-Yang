17fd2c25798e10654d074a509a35577a6dce56d8
l0drex <l0drex@pm.me>
2022-10-23 14:12:04 +0200

    Fix build order

--- scripts/build_ui.sh
+++ scripts/build_ui.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# resource file
-pyside6-rcc ./resources/resources.qrc -o ./src/yin_yang/resources_rc.py
 # ui file from qt designer
 pyside6-uic ./designer/main_window.ui > ./src/yin_yang/ui/main_window.py
-# extract strings to translate (doesn't work with .pro file unfortunately)
+# extract strings to translate
 pyside6-lupdate ./designer/main_window.ui ./src/yin_yang/ui/main_window_connector.py \
-  -ts resources/translations/yin_yang.*.ts -no-obsolete
+  -ts ./resources/translations/yin_yang.*.ts -no-obsolete
 # generate binary translation files
 pyside6-lrelease ./resources/translations/yin_yang.*.ts
+# resource file
+pyside6-rcc ./resources/resources.qrc -o ./src/yin_yang/resources_rc.py
